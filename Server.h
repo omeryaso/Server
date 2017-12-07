ca0ac8b789860c7030467fdf4d2582fd66fcc558
omeryaso <omeryaso@gmail.com>
2017-12-07 16:05:47 +0200

    server class - first revision

---
+++ Server.h
@@ -0,0 +1,22 @@
+//
+// Created by shlomy on 06/12/17.
+//
+
+#ifndef SERVER_SERVER_H
+#define SERVER_SERVER_H
+
+
+class Server {
+public:
+    Server(int port);
+    void start();
+    void stop();
+private:
+    int port;
+    int serverSocket; // the socket's file descriptor
+    void handleClient(int clientSocket);
+    int calc(int arg1, const char op, int arg2) const;
+};
+
+
+#endif //SERVER_SERVER_H
