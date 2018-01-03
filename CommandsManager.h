2802bc44902e8dd30ea7a5d6fd94578f015212f9
Shlomy Trajber <shlomytra@gmail.com>
2018-01-03 22:21:19 +0200

    Added documantation to CommandsManager

--- CommandsManager.h
+++ CommandsManager.h
@@ -12,7 +12,22 @@
 class CommandsManager {
 
 public:
+    /**
+    * @name : *getInstance
+    * @parameters : ignored
+    * @return : returns an instance of CommandsManager (creates new one if not exist)
+    **/
     static CommandsManager* getInstance();
+
+    /**
+    * @name : executeCommand
+    * @parametars : command - the string of the command that we want to use
+    *               args - vector of args that we want to pass (first one usually a command)
+    *               socket- the number of the socket that is sent
+    *              threadId - the id of the thread that we started
+    * @brief : gets the argument from the command and execute them according to the command it gets in the string
+    * @return : ignored
+    **/
     void executeCommand(string command, vector<string> args, int socket = 0, pthread_t* threadId = 0);
 
 private:
